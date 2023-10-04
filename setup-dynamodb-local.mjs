843e2aab77a0dd625266355602bc2ee824b19943
dakota002 <dakota.d@live.com>
2023-10-04 09:13:19 -0400

    Async in the setup

--- setup-dynamodb-local.mjs
+++ setup-dynamodb-local.mjs
@@ -1,4 +1,5 @@
-import { existsSync, mkdirSync } from 'fs'
+import { mkdir } from 'fs/promises'
+import { existsSync } from 'fs'
 import { execSync } from 'child_process'
 
 const dynamodbDir = 'dynamodb_local_latest'
@@ -6,19 +7,23 @@ const dynamodbArchiveURL =
   'https://s3.us-west-2.amazonaws.com/dynamodb-local/dynamodb_local_latest.tar.gz'
 
 // Check if the dynamodbDir directory exists
-if (!existsSync(dynamodbDir)) {
-  // If it doesn't exist, create it
-  mkdirSync(dynamodbDir, { recursive: true })
+async function runSetup() {
+  if (!existsSync(dynamodbDir)) {
+    // If it doesn't exist, create it
+    await mkdir(dynamodbDir, { recursive: true })
 
-  // Download and extract DynamoDB Local
-  try {
-    console.log('Downloading DynamoDB Local...')
-    execSync(`curl -L ${dynamodbArchiveURL} | tar -xz -C ${dynamodbDir}`, {
-      stdio: 'inherit',
-    })
-    console.log('DynamoDB Local setup completed successfully.')
-  } catch (error) {
-    console.error('Error setting up DynamoDB Local:', error.message)
-    process.exit(1)
+    // Download and extract DynamoDB Local
+    try {
+      console.log('Downloading DynamoDB Local...')
+      execSync(`curl -L ${dynamodbArchiveURL} | tar -xz -C ${dynamodbDir}`, {
+        stdio: 'inherit',
+      })
+      console.log('DynamoDB Local setup completed successfully.')
+    } catch (error) {
+      console.error('Error setting up DynamoDB Local:', error.message)
+      process.exit(1)
+    }
   }
 }
+
+runSetup()
