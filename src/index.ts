4d4e373463813d45826d5ec2188cbdfee235319c
dakota002 <dakota.d@live.com>
2023-10-10 14:16:07 -0400

    Implements the feedback from PerRowVersioning PR

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,14 @@ import type {
   PutCommandInput,
   UpdateCommandInput,
 } from '@aws-sdk/lib-dynamodb'
-import type { NativeAttributeValue } from '@aws-sdk/util-dynamodb'
+import { NativeAttributeValue } from '@aws-sdk/util-dynamodb'
 
 export interface DynamoDBAutoIncrementProps {
   /** a DynamoDB document client instance */
   doc: DynamoDBDocument
+
+  /** whether to copy all of the attributes from the table to the counterTable */
+  counterTableCopyItem?: boolean
 
   /** the name of the table in which to store the last value of the counter */
   counterTableName: string
@@ -16,8 +19,9 @@ export interface DynamoDBAutoIncrementProps {
   /** the partition key in the table in which to store the last value of the counter */
   counterTableKey: Record<string, NativeAttributeValue>
 
-  /** the name of the attribute in the table in which to store the last value of the counter */
-  counterTableAttributeName: string
+  /** the name of the attribute in the table in which to store the last value of the counter.
+   * If undefined, defaults to tableAttributeName. */
+  counterTableAttributeName?: string
 
   /** the name of the table in which to store items */
   tableName: string
@@ -60,85 +64,244 @@ export interface DynamoDBAutoIncrementProps {
  * ```
  */
 export class DynamoDBAutoIncrement {
-  constructor(readonly props: DynamoDBAutoIncrementProps) {}
-
+  constructor(readonly props: DynamoDBAutoIncrementProps) {
+    this.#attributeName =
+      this.props.counterTableAttributeName ?? this.props.tableAttributeName
+  }
+  #attributeName: string
   async getLast(): Promise<number | undefined> {
     return (
       (
         await this.props.doc.get({
-          AttributesToGet: [this.props.counterTableAttributeName],
+          AttributesToGet: [this.#attributeName],
           Key: this.props.counterTableKey,
           TableName: this.props.counterTableName,
         })
-      ).Item?.[this.props.counterTableAttributeName] ?? undefined
+      ).Item?.[this.#attributeName] ?? undefined
+    )
+  }
+
+  async getLastItem() {
+    return (
+      (
+        await this.props.doc.get({
+          TableName: this.props.tableName,
+          Key: this.props.counterTableKey,
+        })
+      ).Item ?? undefined
     )
   }
 
   async put(item: Record<string, NativeAttributeValue>) {
-    for (;;) {
-      const counter = await this.getLast()
+    let nextCounter: number
+    let Update: UpdateCommandInput & { UpdateExpression: string }
 
-      let nextCounter: number
-      let Update: UpdateCommandInput & { UpdateExpression: string }
+    if (this.props.counterTableCopyItem) {
+      const ExpressionAttributeNames = this.#getExpressionAttributeNames(item)
+      const ExpressionAttributeValues = this.#getExpressionAttributeValues(item)
+      const UpdateExpression = this.#getUpdateExpression(item)
+      for (;;) {
+        const previousItem = await this.getLastItem()
+        const counter = previousItem?.[this.#attributeName]
 
-      if (counter === undefined) {
-        nextCounter = this.props.initialValue
-        Update = {
-          ConditionExpression: 'attribute_not_exists(#counter)',
-          ExpressionAttributeNames: {
-            '#counter': this.props.counterTableAttributeName,
-          },
-          ExpressionAttributeValues: {
-            ':nextCounter': nextCounter,
-          },
-          Key: this.props.counterTableKey,
-          TableName: this.props.counterTableName,
-          UpdateExpression: 'SET #counter = :nextCounter',
-        }
-      } else {
-        nextCounter = counter + 1
-        Update = {
-          ConditionExpression: '#counter = :counter',
-          ExpressionAttributeNames: {
-            '#counter': this.props.counterTableAttributeName,
-          },
-          ExpressionAttributeValues: {
-            ':counter': counter,
-            ':nextCounter': nextCounter,
-          },
-          Key: this.props.counterTableKey,
-          TableName: this.props.counterTableName,
-          UpdateExpression: 'SET #counter = :nextCounter',
-        }
-      }
-
-      const Put: PutCommandInput = {
-        ConditionExpression: 'attribute_not_exists(#counter)',
-        ExpressionAttributeNames: { '#counter': this.props.tableAttributeName },
-        Item: { [this.props.tableAttributeName]: nextCounter, ...item },
-        TableName: this.props.tableName,
-      }
-
-      if (this.props.dangerously) {
-        await Promise.all([
-          this.props.doc.update(Update),
-          this.props.doc.put(Put),
-        ])
-      } else {
-        try {
-          await this.props.doc.transactWrite({
-            TransactItems: [{ Update }, { Put }],
-          })
-        } catch (e) {
-          if (e instanceof TransactionCanceledException) {
-            continue
-          } else {
-            throw e
+        if (counter === undefined) {
+          nextCounter = this.props.initialValue + 1
+          Update = {
+            ConditionExpression: 'attribute_not_exists(#counter)',
+            ExpressionAttributeNames: {
+              ...ExpressionAttributeNames,
+              '#counter': this.#attributeName,
+            },
+            ExpressionAttributeValues: {
+              ...ExpressionAttributeValues,
+              ':nextCounter': nextCounter,
+            },
+            Key: this.props.counterTableKey,
+            TableName: this.props.tableName,
+            UpdateExpression,
+          }
+        } else {
+          nextCounter = counter + 1
+          Update = {
+            ConditionExpression: '#counter = :counter',
+            ExpressionAttributeNames: {
+              ...ExpressionAttributeNames,
+              '#counter': this.#attributeName,
+            },
+            ExpressionAttributeValues: {
+              ...ExpressionAttributeValues,
+              ':counter': counter,
+              ':nextCounter': nextCounter,
+            },
+            Key: this.props.counterTableKey,
+            TableName: this.props.tableName,
+            UpdateExpression,
           }
         }
-      }
 
-      return nextCounter
+        // PUT should be done to this.props.counterTableName
+        const Put: PutCommandInput = {
+          ConditionExpression: `${this.#getKeyConditionExpression(
+            this.props.counterTableKey
+          )} AND attribute_not_exists(#counter)`,
+          ExpressionAttributeNames: {
+            ...this.#getKeyExpressionAttributeNames(this.props.counterTableKey),
+            '#counter': this.props.tableAttributeName,
+          },
+          Item: {
+            ...previousItem,
+            [this.props.tableAttributeName]: counter ?? 1,
+          },
+          TableName: this.props.counterTableName,
+        }
+
+        if (this.props.dangerously) {
+          const temp = (
+            await this.props.doc.query({
+              KeyConditionExpression: 'widgetID = :widgetID',
+              ExpressionAttributeValues: {
+                ':widgetID': 1,
+              },
+              TableName: this.props.counterTableName,
+            })
+          ).Items
+          console.log(`History: ${JSON.stringify(temp, null, 2)}`)
+          console.log(Put)
+          await Promise.all([
+            this.props.doc.update(Update),
+            this.props.doc.put(Put),
+          ])
+        } else {
+          try {
+            await this.props.doc.transactWrite({
+              TransactItems: [{ Update }, { Put }],
+            })
+          } catch (e) {
+            if (e instanceof TransactionCanceledException) {
+              continue
+            } else {
+              throw e
+            }
+          }
+        }
+
+        return nextCounter
+      }
+    } else {
+      for (;;) {
+        const counter = await this.getLast()
+
+        if (counter === undefined) {
+          nextCounter = this.props.initialValue
+          Update = {
+            ConditionExpression: 'attribute_not_exists(#counter)',
+            ExpressionAttributeNames: {
+              '#counter': this.#attributeName,
+            },
+            ExpressionAttributeValues: {
+              ':nextCounter': nextCounter,
+            },
+            Key: this.props.counterTableKey,
+            TableName: this.props.counterTableName,
+            UpdateExpression: 'SET #counter = :nextCounter',
+          }
+        } else {
+          nextCounter = counter + 1
+          Update = {
+            ConditionExpression: '#counter = :counter',
+            ExpressionAttributeNames: {
+              '#counter': this.#attributeName,
+            },
+            ExpressionAttributeValues: {
+              ':counter': counter,
+              ':nextCounter': nextCounter,
+            },
+            Key: this.props.counterTableKey,
+            TableName: this.props.counterTableName,
+            UpdateExpression: 'SET #counter = :nextCounter',
+          }
+        }
+
+        const Put: PutCommandInput = {
+          ConditionExpression: 'attribute_not_exists(#counter)',
+          ExpressionAttributeNames: {
+            '#counter': this.props.tableAttributeName,
+          },
+          Item: { [this.props.tableAttributeName]: nextCounter, ...item },
+          TableName: this.props.tableName,
+        }
+
+        if (this.props.dangerously) {
+          await Promise.all([
+            this.props.doc.update(Update),
+            this.props.doc.put(Put),
+          ])
+        } else {
+          try {
+            await this.props.doc.transactWrite({
+              TransactItems: [{ Update }, { Put }],
+            })
+          } catch (e) {
+            if (e instanceof TransactionCanceledException) {
+              continue
+            } else {
+              throw e
+            }
+          }
+        }
+
+        return nextCounter
+      }
     }
+  }
+
+  #getKeyConditionExpression(item: Record<string, NativeAttributeValue>) {
+    return Object.keys(item)
+      .map((key) => `attribute_not_exists(#${key})`)
+      .join(' AND ')
+  }
+
+  #getExpressionAttributeValues(item: Record<string, NativeAttributeValue>) {
+    const result: Record<string, NativeAttributeValue> = {}
+    Object.keys(item)
+      // Filter to remove the Partition Key from the ExpressionAttributes
+      .filter((x) => !Object.keys(this.props.counterTableKey).includes(x))
+      .forEach((key) => {
+        result[`:${key}`] = item[key]
+      })
+    return result
+  }
+
+  #getKeyExpressionAttributeNames(item: Record<string, NativeAttributeValue>) {
+    const result: Record<string, string> = {}
+    Object.keys(item).forEach((key) => {
+      result[`#${key}`] = key
+    })
+    return result
+  }
+
+  #getExpressionAttributeNames(item: Record<string, NativeAttributeValue>) {
+    const result: Record<string, string> = {}
+    Object.keys(item)
+      // Filter to remove the Partition Key from the ExpressionAttributes
+      .filter((x) => !Object.keys(this.props.counterTableKey).includes(x))
+      .forEach((key) => {
+        result[`#${key}`] = key
+      })
+    return result
+  }
+
+  #getUpdateExpression(item: Record<string, NativeAttributeValue>): string {
+    let result = 'SET #counter = :nextCounter'
+    if (this.props.counterTableCopyItem) {
+      // Update happens on this.props.tableName
+      const keyArray = Object.keys(item)
+        // Filter to remove the Partition Key from the UpdateExpression
+        .filter((x) => !Object.keys(this.props.counterTableKey).includes(x))
+        .map((key) => `#${key} = :${key}`)
+      keyArray.splice(0, 0, 'SET #counter = :nextCounter')
+      result = keyArray.join(', ')
+    }
+    return result
   }
 }
--- src/test.ts
+++ src/test.ts
@@ -9,6 +9,9 @@ import { DynamoDBAutoIncrement } from '.'
 let doc: DynamoDBDocument
 let autoincrement: DynamoDBAutoIncrement
 let autoincrementDangerously: DynamoDBAutoIncrement
+let autoincrementVersion: DynamoDBAutoIncrement
+let autoincrementVersionDangerously: DynamoDBAutoIncrement
+
 const N = 20
 
 beforeAll(async () => {
@@ -44,18 +47,49 @@ afterEach(async () => {
     [
       { TableName: 'autoincrement', KeyAttributeName: 'tableName' },
       { TableName: 'widgets', KeyAttributeName: 'widgetID' },
-    ].map(
-      async ({ TableName, KeyAttributeName }) =>
-        await Promise.all(
-          ((await doc.scan({ TableName })).Items ?? []).map(
-            async ({ [KeyAttributeName]: KeyValue }) =>
-              await doc.delete({
-                TableName,
-                Key: { [KeyAttributeName]: KeyValue },
-              })
+    ]
+      .map(
+        async ({ TableName, KeyAttributeName }) =>
+          await Promise.all(
+            ((await doc.scan({ TableName })).Items ?? []).map(
+              async ({ [KeyAttributeName]: KeyValue }) =>
+                await doc.delete({
+                  TableName,
+                  Key: { [KeyAttributeName]: KeyValue },
+                })
+            )
           )
+      )
+      .concat(
+        ...[
+          {
+            TableName: 'widgetHistory',
+            PartitionKeyAttributeName: 'widgetID',
+            SortKeyAttributeName: 'version',
+          },
+        ].map(
+          async ({
+            TableName,
+            PartitionKeyAttributeName,
+            SortKeyAttributeName,
+          }) =>
+            await Promise.all(
+              ((await doc.scan({ TableName })).Items ?? []).map(
+                async ({
+                  [PartitionKeyAttributeName]: KeyValue,
+                  [SortKeyAttributeName]: SortKeyValue,
+                }) =>
+                  await doc.delete({
+                    TableName,
+                    Key: {
+                      [PartitionKeyAttributeName]: KeyValue,
+                      [SortKeyAttributeName]: SortKeyValue,
+                    },
+                  })
+              )
+            )
         )
-    )
+      )
   )
 })
 
@@ -101,6 +135,149 @@ describe('dynamoDBAutoIncrement', () => {
     const ids = Array.from(Array(N).keys()).map((i) => i + 1)
     const result = await Promise.all(ids.map(() => autoincrement.put({})))
     expect(result.sort()).toEqual(ids.sort())
+  })
+
+  test('increments version', async () => {
+    const widgetID = 1
+    const item = { description: 'widget A' }
+
+    autoincrementVersion = new DynamoDBAutoIncrement({
+      doc, // DDB document client instance
+      counterTableCopyItem: true, //
+      counterTableName: 'widgetHistory',
+      counterTableKey: { widgetID }, // circularId for the Circular that we are editing
+      tableName: 'widgets',
+      tableAttributeName: 'version',
+      initialValue: 1,
+    })
+
+    // Insert 1st item into table
+    const insertResult = await autoincrement.put(item)
+    expect(insertResult).toBe(widgetID)
+
+    // Verify getLast matches latest insertResult
+    const latestInsertResult = await autoincrement.getLast()
+    expect(latestInsertResult).toBe(widgetID) // widgetID = 1
+
+    // item inserted with autoincrement, version should be undefined
+    const latestItem = await autoincrementVersion.getLastItem()
+    expect(latestItem).toStrictEqual({ ...item, widgetID })
+
+    // Update version in widgets, latestItem.verison is undefined,
+    // add new row to widgetHistory
+    const newWidgetVersion = await autoincrementVersion.put({
+      widgetID,
+      description: 'widget A updated',
+    })
+    expect(newWidgetVersion).toBe(2)
+    const newWidget = await autoincrementVersion.getLastItem()
+    expect(newWidget).toStrictEqual({
+      widgetID,
+      description: 'widget A updated',
+      version: 2,
+    })
+
+    // Update version in widgets agian, latestItem.version should be 2
+    const newVersion = {
+      widgetID,
+      description: 'widget A V3',
+    }
+    const widgetV3Version = await autoincrementVersion.put(newVersion)
+    expect(widgetV3Version).toBe(3)
+    const widgetV3 = await autoincrementVersion.getLastItem()
+    expect(widgetV3).toStrictEqual({
+      ...newVersion,
+      version: 3,
+    })
+
+    const widgetHistory = await doc.query({
+      TableName: autoincrementVersion.props.counterTableName,
+      KeyConditionExpression: 'widgetID = :widgetID',
+      ExpressionAttributeValues: {
+        ':widgetID': 1,
+      },
+    })
+
+    expect(widgetHistory.Count).toBe(2)
+    if (!widgetHistory.Items) return false
+    // count being > 0 means Items should not be empty
+    expect(widgetHistory.Items[0]).toStrictEqual({
+      ...item,
+      widgetID,
+      version: 1,
+    })
+  })
+  test('increments version dangerously', async () => {
+    const widgetID = 1
+    const item = { description: 'widget A dangerous initial' }
+
+    autoincrementVersionDangerously = new DynamoDBAutoIncrement({
+      doc, // DDB document client instance
+      counterTableCopyItem: true, //
+      counterTableName: 'widgetHistory',
+      counterTableKey: { widgetID }, // circularId for the Circular that we are editing
+      tableName: 'widgets',
+      tableAttributeName: 'version',
+      initialValue: 1,
+      dangerously: true,
+    })
+
+    // Insert 1st item into table
+    const insertResult = await autoincrement.put(item)
+    expect(insertResult).toBe(widgetID)
+
+    // Verify getLast matches latest insertResult
+    const latestInsertResult = await autoincrement.getLast()
+    expect(latestInsertResult).toBe(widgetID) // widgetID = 1
+
+    // item inserted with autoincrement, version should be undefined
+    const latestItem = await autoincrementVersionDangerously.getLastItem()
+    expect(latestItem).toStrictEqual({ ...item, widgetID })
+
+    // Update version in widgets, latestItem.verison is undefined,
+    // add new row to widgetHistory
+    const newWidgetVersion = await autoincrementVersionDangerously.put({
+      widgetID,
+      description: 'widget A dangerous updated',
+    })
+    expect(newWidgetVersion).toBe(2)
+    const newWidget = await autoincrementVersionDangerously.getLastItem()
+    expect(newWidget).toStrictEqual({
+      widgetID,
+      description: 'widget A dangerous updated',
+      version: 2,
+    })
+
+    // Update version in widgets agian, latestItem.version should be 2
+    const newVersion = {
+      widgetID,
+      description: 'widget A V3',
+    }
+    const widgetV3Version =
+      await autoincrementVersionDangerously.put(newVersion)
+    expect(widgetV3Version).toBe(3)
+    const widgetV3 = await autoincrementVersionDangerously.getLastItem()
+    expect(widgetV3).toStrictEqual({
+      ...newVersion,
+      version: 3,
+    })
+
+    const widgetHistory = await doc.query({
+      TableName: autoincrementVersionDangerously.props.counterTableName,
+      KeyConditionExpression: 'widgetID = :widgetID',
+      ExpressionAttributeValues: {
+        ':widgetID': 1,
+      },
+    })
+
+    expect(widgetHistory.Count).toBe(2)
+    if (!widgetHistory.Items) return false
+    // count being > 0 means Items should not be empty
+    expect(widgetHistory.Items[0]).toStrictEqual({
+      ...item,
+      widgetID,
+      version: 1,
+    })
   })
 })
 
